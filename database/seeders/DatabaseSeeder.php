85c8949ac57cd6146714bbebcc9738f8be137ad0
Dumpling1902 <rmiadamaris@gmail.com>
2025-11-24 11:49:32 -0600

    Dashboard and install dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        //Llamar al RoleSeeder creado
+        $this->call(RoleSeeder::class);
         // User::factory(10)->create();
+        
 
+        //Crear un usuario de prueba
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Damaris Ramirez',
+            'email' => 'mdamaris1902@gmail.com',
+            'password' => bcrypt('Liliana190206'),
         ]);
     }
 }
---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,14 @@
+<x-admin-layout title="Roles | DentalSys"
+:breadcrumbs="[
+    [
+        'name => 'Dashboard',
+        'route => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Roles',
+    ],
+    "
+
+    >
+
+    </x-admin-layout>
